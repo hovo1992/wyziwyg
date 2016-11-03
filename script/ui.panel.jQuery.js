8602f45ec0cefecfee47037830b911cac7bc2cc4
Hovo <kazhoyan1992@gmail.com>
2016-11-03 12:08:44 +0400

    wyziwygPart1

---
+++ script/ui.panel.jQuery.js
@@ -0,0 +1,23 @@
+﻿(function (global, factory, undefined) {
+
+	global.Panel = factory;
+
+	global.Panel.defaultOptions = {
+		panel: {
+			tagName: 'panel'
+		}
+	};
+
+	global.Panel.prototype = global.Wrapper.prototype;
+
+})('undefined' === typeof (window) ? this : window,
+function (options) {
+
+	'use strict';
+
+	var that = Wrapper.call(this, Panel.defaultOptions.panel, options.panel);
+
+	that.appendTo(options.container);
+
+	return that;
+});
---
+++ script/ui.wrapper.jQuery.js
@@ -0,0 +1,41 @@
+﻿(function (global, factory, undefined) {
+
+	global.Wrapper = factory;
+
+})('undefined' === typeof (window) ? this : window,
+function (defaultOptions, options) {
+
+	'use strict';
+
+	options = $.extend(defaultOptions, options);
+
+	var that = this;
+
+	that.$element = $(`<${options.tagName}>`);
+
+	that.append = (...elements) => {
+		elements.filter((element) => that.$element.append(element.$element || element));
+
+		return that;
+	};
+
+	that.appendTo = (element) => {
+		that.$element.appendTo(element.$element || element);
+
+		return that;
+	};
+
+	that.attr = (json) => {
+		that.$element.attr(json);
+
+		return that;
+	};
+
+	that.css = (json) => {
+		that.$element.css(json);
+
+		return that;
+	};
+
+	return that;
+});
